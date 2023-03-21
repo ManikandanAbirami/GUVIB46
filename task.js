1d5402b28b2cffdc414841448fb01f2612bab6e4
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-03-21 10:03:28 +0530

    questions

--- task.js
+++ task.js
@@ -8,14 +8,35 @@ var arr = [{name: "Manikandan", age:32},{name:"John", age:22}];
 
 
 var xhr = new XMLHttpRequest();//Create Object
-xhr.open('GET', 'https://restcountries.com/v3.1/all');//Access API from the server
+xhr.open('GET', 'https://api.publicapis.org/entries');//Access API from the server
 xhr.responseType = 'json';//Set the response type
 
 xhr.send();//Make sure response is sent back
 
 xhr.onload = function () { //Execute a function once all the loading and return funcitonality is completed
   var responseObj = xhr.response;
-  for(var i = 0; i < responseObj.length; i++) {
-    console.log(responseObj[i].flag)
+  // console.log(responseObj);
+  for(var i = 0; i < responseObj.entries.length; i++) {
+    // console.log(responseObj.entries[i].Description)
   }
 }
+
+var names = ["Mani", "John", "Jill"];
+var result = "";
+
+for(var i = 0; i < names.length; i++){
+  result += names[i] + " ";
+}
+
+console.log(result);
+
+// var arr = ["23 1"];
+// var splitArr = arr[0].split(" ");
+// var a = splitArr[0];
+// var b = splitArr[1];
+// if(a>b){
+
+// }
+// else if () {
+
+// }
