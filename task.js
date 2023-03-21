2ae157caf7a3e83ec6e4a972820124e3478e01c9
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-03-21 08:45:27 +0530

    class 5

--- task.js
+++ task.js
@@ -1,16 +1,21 @@
-var user = { name: "person", age: 5 };
-var user1 = { age: 5, name: "person" };
+// var user = { name: "person", age: 5 };
+// var user1 = { age: 5, name: "person" };
 
 // console.log(user[0]);
 
+var arr = [{name: "Manikandan", age:32},{name:"John", age:22}];
+// console.log("Sample: " + arr[0].name + " " + arr[0].age);
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://restcountries.com/v3.1/all');
-xhr.responseType = 'json';
 
-xhr.send();
+var xhr = new XMLHttpRequest();//Create Object
+xhr.open('GET', 'https://restcountries.com/v3.1/all');//Access API from the server
+xhr.responseType = 'json';//Set the response type
 
-xhr.onload = function () {
+xhr.send();//Make sure response is sent back
+
+xhr.onload = function () { //Execute a function once all the loading and return funcitonality is completed
   var responseObj = xhr.response;
-  console.log(JSON.stringify(responseObj));
+  for(var i = 0; i < responseObj.length; i++) {
+    console.log(responseObj[i])
+  }
 }
