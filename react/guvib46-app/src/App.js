67353402312a70c733d89638417a294c56be87a5
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-05-09 10:15:31 +0530

    interview questions

--- react/guvib46-app/src/App.js
+++ react/guvib46-app/src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div></div>;
+  let moduleName = "GUVI B46 First React app!!!!";
+  return (
+    <div>
+      <div className="header">Guvi Phone Directory</div>
+      <button> Add </button>
+      <div>
+        <span>{moduleName}</span>
+        <br />
+        {/* <span> Name </span>
+        <br />
+        <span> Phone </span> */}
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Type here"
+          defaultValue="Guvi-B46"
+        ></input>
+      </div>
+    </div>
+  );
 }
 
 export default App;
