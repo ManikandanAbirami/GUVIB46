58a804ab72088fb7899667efb93309266e1383ca
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-05-11 08:49:31 +0530

    rendering

--- react/guvib46-app/src/App.js
+++ react/guvib46-app/src/App.js
@@ -4,13 +4,62 @@ import "./App.css";
 
 class App extends Component {
   render() {
+    let users = [
+      {
+        name: "John",
+        phone: "2323232323",
+      },
+      {
+        name: "Jill",
+        phone: "6756765678",
+      },
+      {
+        name: "Mars",
+        phone: "3434343434",
+      },
+      {
+        name: "Jupi",
+        phone: "43434343434",
+      },
+    ];
     return (
       <div>
         <Header />
-        <button className="custom-btn"> Add </button>
-        <div>
-          <span>Name</span> <br></br>
-          <span>Phone</span>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn"> Add </button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span> <br></br>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {/* <div className="grid-container">
+            <span className="grid-item"></span> <br></br>
+            <span className="grid-item"></span>
+          </div>
+
+          <div className="grid-container">
+            <span className="grid-item"></span> <br></br>
+            <span className="grid-item"></span>
+          </div>
+
+          <div className="grid-container">
+            <span className="grid-item"></span> <br></br>
+            <span className="grid-item"></span>
+          </div>
+
+          <div className="grid-container">
+            <span className="grid-item"></span> <br></br>
+            <span className="grid-item"></span>
+          </div> */}
+
+          {users.map((user) => {
+            return (
+              <div className="grid-container">
+                <span className="grid-item">{user.name}</span>
+                <span className="grid-item">{user.phone}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
