53527fc1144cb38adb698e992ab57c47d44a7813
Manikandan Anbalagan <sundaresansubramanyan@analyticbrains.com>
2023-06-06 10:36:05 +0530

    nodejs

--- firstapp/app.js
+++ firstapp/app.js
@@ -1,5 +1,18 @@
-function helloWorld(name) {
-  console.log("Hello world, " + name);
-}
+const express = require("express");
+const app = express();
 
-helloWorld("Guvi B46!!!!");
+const port = 3010;
+
+app.get("/", (req, res) => {
+  res.send("Hello world from Node application!!");
+});
+
+app.get("/read", (req, res) => {
+  res.send("Navigated to Read node API!!!!!");
+});
+
+app.listen(port, () => {
+  console.log("This Node application is running on port " + port);
+});
+
+//app.listen(arg1, callback_function);
